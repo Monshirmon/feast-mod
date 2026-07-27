8263a75cd74b1e8853190a5b5cd5a3a828edc4c1
Monshirmon <714230014@qq.com>
2026-07-27 23:30:51 +0800

    fix: Restore missing Level import in BaiWeiItem

--- src/main/java/com/etherealfeast/item/BaiWeiItem.java
+++ src/main/java/com/etherealfeast/item/BaiWeiItem.java
@@ -10,6 +10,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
 
 import java.util.List;
 
